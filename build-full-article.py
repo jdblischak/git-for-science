46dd993cdd44954ab69d8c201a844e11725b1741
John Blischak <jdblischak@gmail.com>
2015-11-29 14:14:44 -0600

    Include properly labeled and captioned (with cprotect) figures into final full article. The figures are added after the section in which they first appear, but not the paragraph. I think this should be sufficient. Fixes #23.

--- build-full-article.py
+++ build-full-article.py
@@ -13,6 +13,12 @@ import textwrap
 import glob
 
 def write_file(fname, blankline = 1):
+    """Write contents of file to standard out.
+
+    Args:
+        fname - the filename (str)
+        blankline - the number of blank lines to add after the text (int)
+    """
     handle = open(fname, "r")
     for line in handle:
         if "Table 1" in line:
@@ -21,6 +27,49 @@ def write_file(fname, blankline = 1):
     handle.close()
     sys.stdout.write("\n" * blankline)
 
+def write_figure(caption, label):
+    """Write labeled figure with the provided caption.
+
+    Args:
+        caption - a list of strings
+        label - a string to be used with \label
+    """
+    sys.stdout.write("\\begin{figure}[h]\n")
+    # Remove \ref macro from figure caption
+    caption[0] = caption[0].split("}.")[1]
+    # Bold face the caption title
+    caption[0] = "\\bf" + caption[0]
+
+    sys.stdout.write("\\cprotect\\caption{{" + caption[0] + "\n")
+    for line in caption[1:]:
+        sys.stdout.write(line)
+
+    # End caption
+    sys.stdout.write("}\n")
+    # Add label
+    sys.stdout.write("\\label{%s}\n"%(label))
+    # End figure
+    sys.stdout.write("\\end{figure}\n\n")
+
+################################################################################
+# Extract figure legends
+################################################################################
+
+d_figs = {}
+for i in range(4):
+    d_figs[i + 1] = []
+
+legend_file = open("figures/figure-legends.tex", "r")
+# Skip subsection macro and blank line
+legend_file.readline()
+legend_file.readline()
+
+i = 1
+for line in legend_file:
+    if line != "\n":
+        d_figs[i] = d_figs[i] + [line]
+    else:
+        i = i + 1
 
 ################################################################################
 # Add header
@@ -111,16 +160,35 @@ sys.stdout.write("""
 sys.stdout.write("\n")
 
 ################################################################################
+# Figure 1 and 2
+################################################################################
+
+write_figure(d_figs[1], label = "fig:Fig1")
+write_figure(d_figs[2], label = "fig:Fig2")
+
+################################################################################
 # Share your code
 ################################################################################
 
 write_file("share-your-code.tex")
 
 ################################################################################
+# Figure 3
+################################################################################
+
+write_figure(d_figs[3], label = "fig:Fig3")
+
+################################################################################
 # Contribute to other projects
 ################################################################################
 
 write_file("contribute-to-other-projects.tex")
+
+################################################################################
+# Figure 4
+################################################################################
+
+write_figure(d_figs[4], label = "fig:Fig4")
 
 ################################################################################
 # Conclusion
